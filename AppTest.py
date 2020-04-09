c378ed9d66ab26092b12bb874d0a853899a9061d
Adeyemi Sobaki <adeyemi.sobaki@gmail.com>
2020-04-09 04:29:26 +0200

    new commit

--- AppTest.py
+++ AppTest.py
@@ -6,4 +6,4 @@ num_three = int(num_one) * int(num_two)
 
 print("multiplying the numbers... " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
 
-print("multiplying the numbers... " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
+print("multiplying the numbers... " + git checkstr(num_one) + " and " + str(num_two) + " gives " + str(num_three))
