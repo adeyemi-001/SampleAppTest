b119c927e7b034141565dee5d49d6633646d240a
Adeyemi Sobaki <adeyemi.sobaki@gmail.com>
2020-04-09 02:59:55 +0200

    changed code

--- AppTest.py
+++ AppTest.py
@@ -4,6 +4,6 @@ num_two = input("Enter number 2 :")
 
 num_three = int(num_one) * int(num_two)
 
-print("multiplying the numbers " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
+print("multiplying the numbers... " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
 
-print("multiplying the numbers " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
+print("multiplying the numbers... " + str(num_one) + " and " + str(num_two) + " gives " + str(num_three))
